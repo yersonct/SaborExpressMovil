d0c3267350dee44deca9183fc1f4797522d87952
CristianJPalma <cristianspalmas@gmail.com>
2026-06-19 11:41:56 -0500

    subir

--- src/app/chef/index.tsx
+++ src/app/chef/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { ChefOrders } from '../../features/chef/components/ChefOrders';
+import  ChefOrders  from '../../features/chef/components/ChefOrders';
 
 export default function ChefScreen() {
   return (
